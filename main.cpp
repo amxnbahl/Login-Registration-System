d789d255a18bd4e0900b4e71951e4e402ea960e8
Aman Bahl <amanbahlcapricorn@gmail.com>
2022-07-10 00:28:52 +0530

    Fixed and added an option to delete credentials

--- main.cpp
+++ main.cpp
@@ -3,19 +3,21 @@
 #include <string>
 #include <filesystem>
 
-int main() {
+int main()
+{
 	std::system("Color 0A");
 
 	std::cout << "\n\t\t\t\t\t---------------------------------------------";
 	std::cout << "\n\t\t\t\t\t|\tLogin and Registration System\t    |";
 	std::cout << "\n\t\t\t\t\t---------------------------------------------";
 
-	std::cout << "\n\n\t\t\t\t\tDo you want to Register ? : [1]\n";
-	std::cout << "\n\t\t\t\t\tor do you want to Login ? : [2]\n";
+	std::cout << "\n\n\t\t\t\t\tRegister : [1]\n";
+	std::cout << "\n\t\t\t\t\tLogin : [2]\n";
+	std::cout << "\n\t\t\t\t\tDelete Credentials : [3]\n";
 
 	std::cout << "\n\t\t\t\t\tYour choice : ";
 
-	int option_chosen;
+	int option_chosen = 0;
 
 	std::cin >> option_chosen;
 
@@ -23,11 +25,12 @@ int main() {
 
 	std::ifstream fin;
 
-	std::string user_name, user_password, user_confirm_password;
+	std::string hashed_user_name, hashed_user_password, user_file ,user_name, user_password, hashed_user_confirm_password, user_confirm_password;
 
 	std::hash<std::string> hash_string;
 
-	switch (option_chosen) {
+	switch (option_chosen)
+	{
 	case 1:
 
 		std::cout << "\n\t\t\t\t\t-----------------------------------\n";
@@ -38,38 +41,50 @@ int main() {
 
 		std::cin >> user_name;
 
-		fin.open(user_name + ".txt");
+		hashed_user_name = hash_string(user_name);
 
-		if (fin) {
+		user_file = hashed_user_name;
+
+		user_file.append(".txt");
+
+		fin.open(user_file);
+
+		if (fin)
+		{
 			system("Color 04");
 			std::cout << "\n\t\t\t\t\tUser with name '" + user_name + "' already exists!!\n\n";
 			exit(0);
 		}
 
-		file.open(user_name + ".txt", std::ios::out);
+		file.open(user_file, std::ios::out);
 
-		if (!file) {
+		if (!file)
+		{
 			system("Color 04");
 			std::cout << "\n\t\t\t\t\tAn error occurred. Please try again later!!";
 			return 0;
 		}
 
-		while (file) {
+		while (file)
+		{
 			std::cout << "\n\t\t\t\t\tEnter a password: ";
 			std::cin >> user_password;
 
 			std::cout << "\n\t\t\t\t\tConfirm your password: ";
 			std::cin >> user_confirm_password;
 
-			if (user_password != user_confirm_password) {
+			if (user_password != user_confirm_password)
+			{
 				system("Color 04");
 				std::cout << "\n\t\t\t\t\tERROR! Both the passwords don't match. Try again!!\n\n";
 				file.close();
-				std::filesystem::remove(user_name + ".txt");
+				std::filesystem::remove(user_file);
 				exit(0);
 			}
-			else {
-				file << hash_string(user_password);
+			else
+			{
+				hashed_user_password = hash_string(user_password);
+				file << hashed_user_password;
 				std::cout << "\n\t\t\t\t\tSUCCESS! You're Successfully registered as a user!!\n\n";
 			}
 
@@ -90,9 +105,15 @@ int main() {
 
 		std::cin >> user_name;
 
-		fin.open(user_name + ".txt");
+		hashed_user_name = hash_string(user_name);
 
-		if (!fin) {
+		user_file.append(hashed_user_name);
+		user_file.append(".txt");
+
+		fin.open(user_file);
+
+		if (!fin)
+		{
 			system("Color 04");
 			std::cout << "\n\t\t\t\t\tERROR! There's no user with the username '" + user_name + "' in the database\n\n";
 			exit(0);
@@ -102,19 +123,74 @@ int main() {
 
 		std::cin >> user_confirm_password;
 
-		while (fin) {
-			getline(fin, user_password);
+		hashed_user_confirm_password = hash_string(user_confirm_password);
+
+		while (fin)
+		{
+			getline(fin, hashed_user_password);
 			break;
 		}
 
 		fin.close();
 
-		if (hash_string(user_password) != hash_string(user_confirm_password)) {
+		if (hashed_user_confirm_password != hashed_user_password)
+		{
 			system("Color 04");
 			std::cout << "\n\t\t\t\t\tERROR! Your password was incorrect. Try again!!\n\n";
 		}
-		else {
+		else
+		{
 			std::cout << "\n\t\t\t\t\tSUCCESS! You've successfully logged in !!\n\n";
+		}
+
+		break;
+
+	case 3:
+		std::cout << "\n\t\t\t\t\t-----------------------------------\n";
+		std::cout << "\t\t\t\t\t|\tCredentials Deletion Process\t  |\n";
+		std::cout << "\t\t\t\t\t-----------------------------------\n";
+
+		std::cout << "\n\t\t\t\t\tEnter your username: ";
+
+		std::cin >> user_name;
+
+		hashed_user_name = hash_string(user_name);
+
+		user_file.append(hashed_user_name);
+		user_file.append(".txt");
+
+		fin.open(user_file);
+
+		if (!fin)
+		{
+			system("Color 04");
+			std::cout << "\n\t\t\t\t\tUser with name '" + user_name + "' doesn't exists!!\n\n";
+			exit(0);
+		}
+
+		std::cout << "\n\t\t\t\t\tEnter your password: ";
+
+		std::cin >> user_confirm_password;
+
+		hashed_user_confirm_password = hash_string(user_confirm_password);
+
+		while (fin)
+		{
+			getline(fin, hashed_user_password);
+			break;
+		}
+
+		fin.close();
+
+		if (hashed_user_password != hashed_user_confirm_password)
+		{
+			system("Color 04");
+			std::cout << "\n\t\t\t\t\tERROR! Your password was incorrect. Try again!!\n\n";
+		}
+		else
+		{
+			std::filesystem::remove(user_file);
+			std::cout << "\n\t\t\t\t\tSUCCESS! Your credentials have been deleted successfully !!\n\n";
 		}
 
 		break;
